de2f750466b07916921f1d0c88e88a2ce7307dcf
arey <adrien.rey@outlook.com>
2024-10-17 17:41:15 +0200

    Add drifting when high speed

--- code/car.gd
+++ code/car.gd
@@ -1,12 +1,31 @@
 extends VehicleBody3D
 
-const MAX_ENGINE_FORCE = 200.0
+const MAX_ENGINE_FORCE = 500.0
 const MAX_BRAKE_FORCE = 10.0
-const MAX_STEERING_ANGLE = 0.3
+const MAX_STEERING_ANGLE = 0.6
+const NORMAL_FRICTION = 5.0
+const DRIFT_FRICTION_REDUCTION_BACK = 0.08
+const DRIFT_FRICTION_REDUCTION_FRONT = 0.1
+const DRIFT_SPEED_THRESHOLD = 20.0
 
 var steering_angle = 0.0
-var engine
+var engine = 0.0
 
+func drift():
+	var speed = linear_velocity.length()
+	var is_drifting = speed > DRIFT_SPEED_THRESHOLD
+	if is_drifting:
+		$"wheel-back-left".wheel_friction_slip = NORMAL_FRICTION * DRIFT_FRICTION_REDUCTION_BACK
+		$"wheel-back-right".wheel_friction_slip = NORMAL_FRICTION * DRIFT_FRICTION_REDUCTION_BACK
+		$"wheel-front-left".wheel_friction_slip = NORMAL_FRICTION * DRIFT_FRICTION_REDUCTION_FRONT
+		$"wheel-front-right".wheel_friction_slip = NORMAL_FRICTION * DRIFT_FRICTION_REDUCTION_FRONT
+	else:
+		pass
+		$"wheel-back-left".wheel_friction_slip = NORMAL_FRICTION
+		$"wheel-back-right".wheel_friction_slip = NORMAL_FRICTION
+		$"wheel-front-left".wheel_friction_slip = NORMAL_FRICTION
+		$"wheel-front-right".wheel_friction_slip = NORMAL_FRICTION
+		
 func _physics_process(delta):
 	var steering_input = 0.0
 	var engine_input = 0.0
@@ -21,6 +40,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("reverse"):
 		engine_input -= 1
 
+	drift()
+	
 	$"wheel-front-left".steering = lerp(steering_angle, steering_input * MAX_STEERING_ANGLE, 0.3)
 	$"wheel-front-right".steering = lerp(steering_angle, steering_input * MAX_STEERING_ANGLE, 0.3)
 	$"wheel-back-left".engine_force = engine_input * MAX_ENGINE_FORCE
@@ -30,4 +51,5 @@ func _physics_process(delta):
 		brake = MAX_BRAKE_FORCE 
 	else:
 		brake = 0	
+		
 	
