0516475fd07e1d6108a4d795dc166f34f5fb517a
arey <adrien.rey@outlook.com>
2024-10-24 15:12:48 +0200

    Car script updated

--- code/car.gd
+++ code/car.gd
@@ -1,16 +1,16 @@
 extends VehicleBody3D
 
 const MAX_SPEED = 25.0
-const MAX_ENGINE_FORCE = 500.0
+const MAX_ENGINE_FORCE = 800.0
 const MAX_BRAKE_FORCE = 10.0
 const MAX_STEERING_ANGLE = 0.6
+const STEERING_SPEED = 0.05
 const NORMAL_FRICTION = 5.0
 const DRIFT_FRICTION_REDUCTION_BACK = 0.08
 const DRIFT_FRICTION_REDUCTION_FRONT = 0.1
 const DRIFT_SPEED_THRESHOLD = 20.0
 
 var steering_angle = 0.0
-var engine = 0.0
 
 func drift():
 	var speed = linear_velocity.length()
@@ -27,6 +27,35 @@ func drift():
 		$"wheel-front-left".wheel_friction_slip = NORMAL_FRICTION
 		$"wheel-front-right".wheel_friction_slip = NORMAL_FRICTION
 		
+func steering(steering_input):
+	if(steering_input != 0):
+		steering_angle += ( STEERING_SPEED * steering_input)
+		
+		if (steering_angle < MAX_STEERING_ANGLE && steering_angle > -MAX_STEERING_ANGLE):
+			$"wheel-front-left".steering = steering_angle
+			$"wheel-front-right".steering = steering_angle
+		else:
+			steering_angle = MAX_STEERING_ANGLE * steering_input
+			$"wheel-front-left".steering = steering_angle
+			$"wheel-front-right".steering = steering_angle
+			
+	else:
+		steering_angle = 0
+		$"wheel-front-left".steering = steering_angle
+		$"wheel-front-right".steering = steering_angle
+		
+func engine(engine_input):
+	if(linear_velocity.length() < MAX_SPEED && engine_input != -1):
+		$"wheel-back-left".engine_force = engine_input * MAX_ENGINE_FORCE
+		$"wheel-back-right".engine_force = engine_input * MAX_ENGINE_FORCE
+	else:
+		$"wheel-back-left".engine_force = 0
+		$"wheel-back-right".engine_force = 0
+	if (engine_input == -1):
+		brake = MAX_BRAKE_FORCE 
+	else:
+		brake = 0	
+		
 func _physics_process(delta):
 	var steering_input = 0.0
 	var engine_input = 0.0
@@ -40,30 +69,14 @@ func _physics_process(delta):
 		engine_input += 1
 	if Input.is_action_pressed("reverse"):
 		engine_input -= 1
-		#$"light-back-left".get_active_material(0).emission_enabled = true
 		$"light-back-left".visible = true
 		$"light-back-right".visible = true
 	else:
 		$"light-back-left".visible = false
 		$"light-back-right".visible = false
 		
-
-		
-
-	drift()
-	
-	$"wheel-front-left".steering = lerp(steering_angle, steering_input * MAX_STEERING_ANGLE, 0.3)
-	$"wheel-front-right".steering = lerp(steering_angle, steering_input * MAX_STEERING_ANGLE, 0.3)
-
-	if(linear_velocity.length() < MAX_SPEED):
-		$"wheel-back-left".engine_force = engine_input * MAX_ENGINE_FORCE
-		$"wheel-back-right".engine_force = engine_input * MAX_ENGINE_FORCE
-	else:
-		$"wheel-back-left".engine_force = 0
-		$"wheel-back-right".engine_force = 0
-	if (engine_input == -1):
-		brake = MAX_BRAKE_FORCE 
-	else:
-		brake = 0	
+	#sdrift()
+	steering(steering_input)
+	engine(engine_input)
 		
 	
