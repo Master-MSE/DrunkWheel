ccf4c81689fa7b8854b407a4d2e9f8074bd1cb16
mathieu schupbach <mathieu.schupbach@gmail.com>
2024-11-10 23:55:47 +0100

    first test
    # Conflicts:
    #	code/car.gd

--- code/car.gd
+++ code/car.gd
@@ -1,109 +1,72 @@
 extends VehicleBody3D
 
-@export var vertical_obstacle_collision_impulse_strength: float = 1.0
-@export var relative_obstacle_collision_impulse_strength: float = 1.0
+const STEER_SPEED = 2
+const STEER_LIMIT = 0.3
+const BRAKE_STRENGTH = 2.0
 
-const MAX_SPEED = 25.0
-const MAX_ENGINE_FORCE = 800.0
-const MAX_BRAKE_FORCE = 10.0
-const MAX_STEERING_ANGLE = 0.6
-const STEERING_SPEED = 0.05
-const NORMAL_FRICTION = 5.0
-const DRIFT_FRICTION_REDUCTION_BACK = 0.08
-const DRIFT_FRICTION_REDUCTION_FRONT = 0.1
-const DRIFT_SPEED_THRESHOLD = 20.0
+@export var engine_force_value := 100.0
 
-var steering_angle = 0.0
+var previous_speed := linear_velocity.length()
+var _steer_target := 0.0
+var score :=0.0
 
-func drift():
-	var speed = linear_velocity.length()
-	var is_drifting = speed > DRIFT_SPEED_THRESHOLD
-	if is_drifting:
-		$"wheel-back-left".wheel_friction_slip = NORMAL_FRICTION * DRIFT_FRICTION_REDUCTION_BACK
-		$"wheel-back-right".wheel_friction_slip = NORMAL_FRICTION * DRIFT_FRICTION_REDUCTION_BACK
-		$"wheel-front-left".wheel_friction_slip = NORMAL_FRICTION * DRIFT_FRICTION_REDUCTION_FRONT
-		$"wheel-front-right".wheel_friction_slip = NORMAL_FRICTION * DRIFT_FRICTION_REDUCTION_FRONT
-	else:
-		pass
-		$"wheel-back-left".wheel_friction_slip = NORMAL_FRICTION
-		$"wheel-back-right".wheel_friction_slip = NORMAL_FRICTION
-		$"wheel-front-left".wheel_friction_slip = NORMAL_FRICTION
-		$"wheel-front-right".wheel_friction_slip = NORMAL_FRICTION
-		
-func steering(steering_input):
-	if(steering_input != 0):
-		steering_angle += ( STEERING_SPEED * steering_input)
-		
-		if (steering_angle < MAX_STEERING_ANGLE && steering_angle > -MAX_STEERING_ANGLE):
-			$"wheel-front-left".steering = steering_angle
-			$"wheel-front-right".steering = steering_angle
+#@onready var desired_engine_pitch: float = $EngineSound.pitch_scale
+
+func _physics_process(delta: float):
+	var fwd_mps := (linear_velocity * transform.basis).x
+
+	_steer_target = Input.get_axis(&"right", &"left")
+	_steer_target *= STEER_LIMIT
+
+	# Engine sound simulation (not realistic, as this car script has no notion of gear or engine RPM).
+	#desired_engine_pitch = 0.05 + linear_velocity.length() / (engine_force_value * 0.5)
+	# Change pitch smoothly to avoid abrupt change on collision.
+	#$EngineSound.pitch_scale = lerpf($EngineSound.pitch_scale, desired_engine_pitch, 0.2)
+
+	if abs(linear_velocity.length() - previous_speed) > 1.0:
+		# Sudden velocity change, likely due to a collision. Play an impact sound to give audible feedback,
+		# and vibrate for haptic feedback.
+		#$ImpactSound.play()
+		Input.vibrate_handheld(100)
+		for joypad in Input.get_connected_joypads():
+			Input.start_joy_vibration(joypad, 0.0, 0.5, 0.1)
+
+	# Automatically accelerate when using touch controls (reversing overrides acceleration).
+	if DisplayServer.is_touchscreen_available() or Input.is_action_pressed(&"forward"):
+		# Increase engine force at low speeds to make the initial acceleration faster.
+		var speed := linear_velocity.length()
+		if speed < 5.0 and not is_zero_approx(speed):
+			engine_force = clampf(engine_force_value * 5.0 / speed, 0.0, 100.0)
 		else:
-			steering_angle = MAX_STEERING_ANGLE * steering_input
-			$"wheel-front-left".steering = steering_angle
-			$"wheel-front-right".steering = steering_angle
-			
-	else:
-		steering_angle = 0
-		$"wheel-front-left".steering = steering_angle
-		$"wheel-front-right".steering = steering_angle
-		
-func engine(engine_input):
-	if(linear_velocity.length() < MAX_SPEED && engine_input != -1):
-		$"wheel-back-left".engine_force = engine_input * MAX_ENGINE_FORCE
-		$"wheel-back-right".engine_force = engine_input * MAX_ENGINE_FORCE
-	else:
-		$"wheel-back-left".engine_force = 0
-		$"wheel-back-right".engine_force = 0
-	if (engine_input == -1):
-		brake = MAX_BRAKE_FORCE 
-	else:
-		brake = 0	
+			engine_force = engine_force_value
 
-func align_y(xform, new_y):
-	xform.basis.y = new_y
-	xform.basis.x = -xform.basis.z.cross(new_y)
-	xform.basis = xform.basis.orthonormalized()
-	return xform
-	
-func _physics_process(delta):
-	var steering_input = 0.0
-	var engine_input = 0.0
-
-	if Input.is_action_pressed("right"):
-		steering_input -= 1
-	if Input.is_action_pressed("left"):
-		steering_input += 1
-	
-	if Input.is_action_pressed("forward"):
-		engine_input += 1
-	if Input.is_action_pressed("reverse"):
-		engine_input -= 1
-		$"light-back-left".visible = true
-		$"light-back-right".visible = true
+		if not DisplayServer.is_touchscreen_available():
+			# Apply analog throttle factor for more subtle acceleration if not fully holding down the trigger.
+			engine_force *= Input.get_action_strength(&"forward")
 	else:
-		$"light-back-left".visible = false
-		$"light-back-right".visible = false
-	
-	if ($"raycast-front".is_colliding() || $"raycast-back".is_colliding()):
-		var nf = $"raycast-front".get_collision_normal() if $"raycast-front".is_colliding() else Vector3.UP
-		var nr = $"raycast-back".get_collision_normal() if $"raycast-back".is_colliding() else Vector3.UP
-		var n = ((nr + nf) / 2.0).normalized()
-		var xform = align_y(global_transform, n)
-		global_transform = global_transform.interpolate_with(xform, 0.1)
-	
-	#drift()
-	steering(steering_input)
-	engine(engine_input)
-		
-	
+		engine_force = 0.0
 
+	if Input.is_action_pressed(&"reverse"):
+		# Increase engine force at low speeds to make the initial reversing faster.
+		var speed := linear_velocity.length()
+		if speed < 5.0 and not is_zero_approx(speed):
+			engine_force = -clampf(engine_force_value * BRAKE_STRENGTH * 5.0 / speed, 0.0, 100.0)
+		else:
+			engine_force = -engine_force_value * BRAKE_STRENGTH
 
-func _on_body_entered(body: Node) -> void:
-	print("Body Entered")
-	if body is RigidBody3D:
-		if body.collision_layer == 2:
-			body.apply_impulse(linear_velocity + 
-			(Vector3.UP * vertical_obstacle_collision_impulse_strength) + 
-			((body.position - position) * relative_obstacle_collision_impulse_strength))
-			
-			print(str(linear_velocity))
+		# Apply analog brake factor for more subtle braking if not fully holding down the trigger.
+		engine_force *= Input.get_action_strength(&"reverse")
+	
+	
+	if Input.is_action_just_pressed(&"alcool_up"):
+		score += 1.0;
+		RenderingServer.global_shader_parameter_set("tauxalcool",score);
+	if Input.is_action_just_pressed(&"alcool_down"):
+		score -= 1.0;
+		RenderingServer.global_shader_parameter_set("tauxalcool",score);
+	
+	
+	
+	steering = move_toward(steering, _steer_target, STEER_SPEED * delta)
+
+	previous_speed = linear_velocity.length()
