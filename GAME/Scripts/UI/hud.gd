1e5cb1c1f1b87a6e2061eaf6528d8999ef0ae926
mathieuSchu <147143876+mathieuSchu@users.noreply.github.com>
2024-12-18 19:52:58 +0100

    add description bar alcool

--- GAME/Scripts/UI/hud.gd
+++ GAME/Scripts/UI/hud.gd
@@ -4,11 +4,13 @@ extends CanvasLayer
 @onready var particles: ColorRect = $Particule
 @onready var alcoolbar_bot: Sprite2D = $Sprite2D
 @onready var alcoolbar_top: Sprite2D = $Sprite2D2
+@onready var alcoolbar_txt: Label = $txt_alcool
 
 
 const base_screen_size = Vector2(1152,642)
 var base_position_color=Vector2(1089,442.5)
 var base_position_particles=Vector2(1030,39)
+var base_position_txt=Vector2(1007,490)
 var base_size_color=Vector2(18,0)
 var actuly_state=false
 
@@ -27,9 +29,10 @@ func updadte_affichage(new_screen_size:Vector2)->void:
 	alcoolbar_top.scale=scaling*0.3
 	alcoolbar.scale=scaling
 	particles.scale=scaling
+	alcoolbar_txt.scale=scaling
 	alcoolbar.position=base_position_color*scaling
 	particles.position=base_position_particles*scaling
-	
+	alcoolbar_txt.position=base_position_txt*scaling
 	
 func set_change_particles(state:bool)->void:
 	if state!=actuly_state:
